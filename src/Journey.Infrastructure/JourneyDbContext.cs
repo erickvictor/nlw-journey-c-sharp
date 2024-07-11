622aa4ac7ed9db897e903061738465897e72754a
Erick Bernardo <erickvictor@gmail.com>
2024-07-11 08:30:32 -0400

    Feature: Endpoint POST and GET to api Trips

---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure;
+
+public class JourneyDbContext : DbContext
+{
+    public DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=/home/geek/backend/nlw-journey-c-sharp/JourneyDatabase.db");
+    }
+}
